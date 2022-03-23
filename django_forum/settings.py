8f21e0f3981098ad799830ab54fbb175cd49a80b
Kelly Safrit <archerywriter@gmail.com>
2022-03-23 18:14:22 -0400

    database added

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,11 +133,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+cloudinary.config( 
+  cloud_name = "kcsafrit", 
+  api_key = "234645422668726", 
+  api_secret = "YSLR3c4nYCzic6LP5VaLE4MKDwM",
+)
+
 
 
 # Default primary key field type
@@ -144,8 +157,5 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-cloudinary.config( 
-  cloud_name = "kcsafrit", 
-  api_key = "234645422668726", 
-  api_secret = "YSLR3c4nYCzic6LP5VaLE4MKDwM",
-)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
